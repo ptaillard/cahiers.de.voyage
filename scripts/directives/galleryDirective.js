c8720a973f4f9d76ebbfc8d96d0816c07cf53213
pierre.taillard <pierre_taillard@yahoo.fr>
2014-05-08 16:10:23 +0200

    Signed-off-by: pierre.taillard <pierre_taillard@yahoo.fr>

--- scripts/directives/galleryDirective.js
+++ scripts/directives/galleryDirective.js
@@ -5,32 +5,36 @@ CahiersDeVoyageApp.directive('cdvGallery', [function() {
 			restrict: 'A',
 			//transclude: 'element',
 			replace: true,
-			controller: function ($scope) {
-				// Set of Photos
-			    $scope.photos = [
-			        {src: './images/media/gallery/ST.jpg', desc: 'Saint Tropez'},
-			        {src: './images/media/gallery/camera.jpg', desc: 'Appareil photo'}
-			    ];
+			controller: function ($scope, $http) {
+			    $scope.photos = [];
 
-			    // initial image index
+
+				$scope.loadImages = function() {
+
+					$http.get('images.json').
+						success(function(data, status, headers, config) {
+					      	$scope.photos = data;
+					    }).
+					    error(function(data, status, headers, config) {
+					      $scope.error = "Problème d'accès au serveur...";
+					    });
+				};
+
 			    $scope._Index = 0;
+			    $scope.loadImages();
 
-			    // if a current image is the same as requested image
 			    $scope.isActive = function (index) {
 			        return $scope._Index === index;
 			    };
 
-			    // show prev image
 			    $scope.showPrev = function () {
 			        $scope._Index = ($scope._Index > 0) ? --$scope._Index : $scope.photos.length - 1;
 			    };
 
-			    // show next image
 			    $scope.showNext = function () {
 			        $scope._Index = ($scope._Index < $scope.photos.length - 1) ? ++$scope._Index : 0;
 			    };
 
-			    // show a certain image
 			    $scope.showPhoto = function (index) {
 			        $scope._Index = index;
 			    };
