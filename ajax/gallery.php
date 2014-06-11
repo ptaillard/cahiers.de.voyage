d16c46aaa6c67f6475cf0dfa6620bf746c7a742a
pierre.taillard <pierre_taillard@yahoo.fr>
2014-06-11 21:58:45 +0200

    Signed-off-by: pierre.taillard <pierre_taillard@yahoo.fr>

--- ajax/gallery.php
+++ ajax/gallery.php
@@ -5,21 +5,25 @@
 
   $it = new DirectoryIterator($dir);
 
-
   foreach ($it as $fileinfo) {
-      if ($fileinfo->isFile()) {
-          $src = $fileinfo->getFilename();
-          $desc = $auteur = "";
-          $sql = "SELECT commentaire, auteur FROM `ouestamericain_commentaire` WHERE image=\"".$src."\"";
-          $req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+    if ($fileinfo->isFile()) {
+      $rawfiles[] = $fileinfo->getFilename();
+    }
+  }
 
-          while ($row = mysql_fetch_assoc($req)) {
-            $desc = $row["commentaire"];
-            $auteur = $row["auteur"];
-          }
+  natcasesort($rawfiles);
 
-          $files[] = array('src' => 'images/media/gallery/'.$src, 'file' => $src, 'desc' => $desc, 'auteur' => $auteur);
-      }
+  foreach($rawfiles as $file) {
+    $desc = $auteur = "";
+    $sql = "SELECT commentaire, auteur FROM `ouestamericain_commentaire` WHERE image=\"".$file."\"";
+    $req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+
+    while ($row = mysql_fetch_assoc($req)) {
+      $desc = $row["commentaire"];
+      $auteur = $row["auteur"];
+    }
+
+    $files[] = array('src' => 'images/media/gallery/'.$file, 'file' => $file, 'desc' => $desc, 'auteur' => $auteur);
   }
   
   header('Content-type: application/json');
